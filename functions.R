68a490cf597d4ca06590b976de623c71f1d01605
Vincent Godard <94741073+VincentGodard@users.noreply.github.com>
2022-03-21 15:54:52 +0100

    Add function to obtain thresholds

--- functions.R
+++ functions.R
@@ -31,3 +31,36 @@ tmp2 = cbind(as.data.frame(pts),df_st_pts)
 pts2 = vect(cbind(tmp2$X,tmp2$Y),type="points",atts=tmp2,crs=crs(pts))
 return(pts2)
 }
+
+
+get_qq_threshold <-function(rast,fact_red=10,plot=FALSE){
+  data = na.omit(as.vector(rast))
+  data = sort(data) # ordering
+  data_sp = data[seq_along(data)%%fact_red==0] # undersampling
+  qq = qqnorm(data,type="l",lwd=3,plot.it = FALSE)
+  qq_sp = qqnorm(data_sp,type="l",lwd=3,plot.it = FALSE)
+  
+  #
+  probs = c(0.25,0.75)
+  vp <- quantile(data, probs = probs)
+  lm1 <- lm(vp~qnorm(probs))
+  # difference
+  b = qq$x*lm1$coefficients[2]+lm1$coefficients[1]
+  diff = abs((b-qq$y)/b)
+  #first value
+  iflag = which(diff <0.01)[1]
+  val = qq$y[iflag]
+  #
+  if (plot){
+    plot(qq_sp$x,qq_sp$y,type="l",lwd=3,
+         main=paste("Threshold=",round(val,2)),
+         xlab="Theoretical quantiles",ylab="Samples quantiles")
+    grid()
+    abline(lm1,col="red",lty=2)
+    abline(v=qq$x[iflag],col="blue")
+    points(qnorm(probs),vp,pch=21,bg="white")
+  }
+  return(val)
+} 
+
+
